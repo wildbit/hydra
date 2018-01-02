6b3893e2d21bc5e2eb7ce8c27a45ffc02b0c808c
Andrew Theken <andrew@wildbit.com>
2018-01-02 15:05:29 -0500

    Further simplication and refinement of the lua script.

--- backend/config/stats.lua
+++ backend/config/stats.lua
@@ -1,13 +1,11 @@
-function trim(s)
- return s:gsub("^%s*(.-)%s*$", "%1")
-end
+-- The following outlines the socket address to which we need to connect.
+-- it should match the unix socket that you defined in your haproxy.cfg
+local socket_address = "/var/run/haproxy.sock"
 
-function run_command(socket_name, command)
-
-	-- send request
+function run_command(command)
 	s = core.tcp()
-	s:connect("unix@" .. trim(socket_name))
-	s:send(trim(command) .. "\n")
+	s:connect("unix@" .. socket_address)
+	s:send(command .. "\n")
 	results = s:receive("*a")
 	s:close()
 	return results
@@ -16,7 +14,7 @@ end
 function socket_command(applet)
 	if applet.method == "POST" and applet.length > 0 then
 		local command = applet:receive()
-		local results = run_command("/var/run/haproxy.sock", command)
+		local results = run_command(command)
 		if results == nil then
 			applet:set_status(404)
 			applet.start_response()
