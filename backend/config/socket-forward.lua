aeacbdf2011aad4a950c2491708abef474557b14
Andrew Theken <andrew@wildbit.com>
2018-01-02 15:02:07 -0500

    Make stats return over http endpoint.

--- backend/config/socket-forward.lua
+++
@@ -1,8 +0,0 @@
-core.register_service("socket-forward", "http", function(applet)
-   local response = "Hello World !"
-   applet:set_status(200)
-   applet:add_header("content-length", string.len(response))
-   applet:add_header("content-type", "text/plain")
-   applet:start_response()
-   applet:send(response)
-end)
--- dev.sh
+++ dev.sh
@@ -2,5 +2,6 @@
 
 pushd backend
 docker build -t haproxui-testing .
-docker run -v "$(pwd)/config":/var/haproxy -p 10000:10000 -p 10001:10001 -t haproxui-testing
+docker rm -f haproxui-testing 2>&1
+docker run --name=haproxui-testing -d -v "$(pwd)/config":/var/haproxy -p 10000:10000 -p 10001:10001 -p 10002:10002 -t haproxui-testing
 popd
