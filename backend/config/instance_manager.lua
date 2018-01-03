2d3c0cfe6a0082953fb6f81d85bdafc01b4fb339
Andrew Theken <andrew@wildbit.com>
2018-01-03 11:28:13 -0500

    Adding backend metadata endpoint.

---
+++ backend/config/instance_manager.lua
@@ -0,0 +1,54 @@
+-- Lua API reference: http://www.arpalert.org/src/haproxy-lua-api/1.7/index.html
+
+-- The following outlines the socket address to which we need to connect.
+-- it should match the unix socket that you defined in your haproxy.cfg
+local socket_address = "/var/run/haproxy.sock"
+
+function run_command(command)
+	s = core.tcp()
+	s:connect("unix@" .. socket_address)
+	s:send(command .. "\n")
+	results = s:receive("*a")
+	s:close()
+	return results
+end
+
+function get_metadata(applet)
+	-- This is a place to include optional metadata about your
+	-- particular instance of HAProxy. Here's the format that 
+	-- should be used: <key_name><space><content>
+	-- HOSTNAME foo.example.com
+	-- This is useful in the case of adding metadata to the UI.
+	return "HOSTNAME ..."
+end
+
+function instance_manager(applet)
+	if applet.method == "POST" and applet.length > 0 and applet.path == "/manage" then
+		local command = applet:receive()
+		local results = run_command(command)
+		if results == nil then
+			applet:set_status(404)
+			applet.start_response()
+			applet.send("No results found.")
+		else
+			applet:set_status(200)
+			applet:add_header("Content-Length", string.len(results))
+			applet:add_header("Content-Type", "text/plain")
+			applet:start_response()
+			applet:send(results)
+		end
+	elseif applet.method == "GET" and applet.path == "/metadata" then
+		applet:set_status(200)
+		results = get_metadata(applet)
+		applet:start_response()
+		applet:add_header("Content-Type", "text/plain")
+		applet:add_header("Content-Length", string.len(results))
+		applet:send(results)
+	else
+		applet:set_status(404)
+		applet:start_response()
+		applet:send("Please send an HAProxy command as the body of a POST request to /manage, or a GET request to /metadata")
+	end
+end
+
+core.register_service("instance_manager", "http", instance_manager)
--- dev.sh
+++ dev.sh
@@ -3,5 +3,5 @@
 pushd backend
 docker build -t haproxui-testing .
 docker rm -f haproxui-testing 2>&1
-docker run --name=haproxui-testing -d -v "$(pwd)/config":/var/haproxy -p 10000:10000 -p 10001:10001 -p 10002:10002 -t haproxui-testing
+docker run --name=haproxui-testing -d -v "$(pwd)/config":/var/haproxy -p 9999:9999 -p 10000:10000 -p 10001:10001 -p 10002:10002 -t haproxui-testing
 popd
