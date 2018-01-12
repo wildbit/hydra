0ca5a605017836250b2b27df81f31605fe40c965
Andrew Theken <andrew@wildbit.com>
2018-01-12 10:33:40 -0500

    Back to using only cloudfront, and allow interface to load through lua.

--- backend/dev.sh
+++ backend/dev.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
 
-docker build -t haproxui-testing .
-docker rm -f haproxui-testing 2>&1
-docker run --name=haproxui-testing -d -v "$(pwd)/config":/var/haproxy -p 9999:9999 -p 10000:10000 -p 10001:10001 -p 10002:10002 -p 10003:10003 -t haproxui-testing
+docker build -t hydra-testing .
+docker rm -f hydra-testing 2>&1
+docker run --name=hydra-testing -d -v "$(pwd)/config":/var/haproxy -p 9999:9999 -p 10000:10000 -p 10001:10001 -p 10002:10002 -p 10003:10003 -t hydra-testing
